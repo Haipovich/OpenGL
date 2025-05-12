178eff101f1f7aa60a88d7ddd62c08a6afb4dcd9
Haipovich <hismatovegor@gmail.com>
2025-05-13 03:21:14 +0500

    Lab1

---
+++ Lab1/build/CMakeFiles/OpenGLSurfaceApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLSurfaceApp.dir/link.d"
+  "CMakeFiles/OpenGLSurfaceApp.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLSurfaceApp.dir/src/main.cpp.o.d"
+  "OpenGLSurfaceApp"
+  "OpenGLSurfaceApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLSurfaceApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
